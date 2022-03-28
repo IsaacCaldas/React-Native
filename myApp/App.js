08b92766a9bedf3834d70436603e0b1e66c79ae7
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-03-28 09:43:04 -0300

    Props

--- myApp/App.js
+++ myApp/App.js
@@ -9,15 +9,28 @@ class App extends Component {
     return (
       <View style={styles.container}>
         <Text>Hello World!</Text>
-        <Image 
-          source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_font_awesome.svg/2048px-User_font_awesome.svg.png'}}
-          style={{ width: 40, height: 40, marginTop: 10 }}
-        />
+        <Guest name='Isaac' />
         <Text style={{ color: '#33FF', fontSize: 20 }}> {/* ou '20px' */}
           <b>{name}</b> is a programmer?
         </Text>
       </View>
     );
+  }
+}
+
+class Guest extends Component {
+  render() {
+    let image = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_f ont_awesome.svg/2048px-User_font_awesome.svg.png'
+    return (
+      <>
+        <Image 
+          source={{uri: image }}
+          style={{ width: 40, height: 40, marginTop: 10 }}
+        />
+        <Text>{this.props.name}</Text>
+      </>
+    )
+
   }
 }
 
