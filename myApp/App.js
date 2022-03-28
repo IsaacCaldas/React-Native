367bd5b8c9f2860ed4b27b32a944d4c28a546760
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-03-28 09:53:55 -0300

    States

--- myApp/App.js
+++ myApp/App.js
@@ -1,36 +1,32 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 class App extends Component {
-  render() {
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: ''
+    }
 
-    let name = 'Isaac'
+    this.login = this.login.bind(this)
+
+  }
+
+  login(name) {
+    this.setState({
+      name
+    })
+  }
+  
+  render() {
 
     return (
       <View style={styles.container}>
-        <Text>Hello World!</Text>
-        <Guest name='Isaac' />
-        <Text style={{ color: '#33FF', fontSize: 20 }}> {/* ou '20px' */}
-          <b>{name}</b> is a programmer?
-        </Text>
+        <Text style={{ fontSize: 20, marginBottom: 20 }}>Hello <b>{this.state.name}</b>!</Text>
+        <Button title='Login' onPress={() => this.login('Isaac')} />
       </View>
     );
-  }
-}
-
-class Guest extends Component {
-  render() {
-    let image = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_f ont_awesome.svg/2048px-User_font_awesome.svg.png'
-    return (
-      <>
-        <Image 
-          source={{uri: image }}
-          style={{ width: 40, height: 40, marginTop: 10 }}
-        />
-        <Text>{this.props.name}</Text>
-      </>
-    )
-
   }
 }
 
