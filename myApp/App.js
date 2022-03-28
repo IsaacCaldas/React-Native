c6882fa1fc53936b522aa887a9b9071aa95d6668
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-03-28 10:07:55 -0300

    Fixed and dinamic sizes

--- myApp/App.js
+++ myApp/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View, Button } from 'react-native';
-
 class App extends Component {
 
   render() {
 
     return (
       <View style={styles.container}>
-        
-        <Text style={styles.firstText}>I am the first text.</Text>
-        <Text style={styles.secondText}>I am the second text.</Text>
-        <Text style={styles.thirdText}>I am the third text.</Text>
-
+        <View style={{flex: 1, backgroundColor: 'red'}}></View>
+        <View style={{flex: 2, backgroundColor: 'yellow'}}></View>
+        <View style={{flex: 1, backgroundColor: 'blue'}}></View>
       </View>
     );
   }
@@ -22,20 +19,6 @@ export default App
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  firstText: {
-    fontSize: 30,
-  },
-  secondText: {
-    color: 'blue',
-    margin: 30
-  },
-  thirdText: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'brown'
-  },
+    backgroundColor: '#111',
+  }
 });
