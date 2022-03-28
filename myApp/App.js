3340918918e1a414528455a99765a848fd6d3b17
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-03-28 09:32:04 -0300

    Props in React-native

--- myApp/App.js
+++ myApp/App.js
@@ -1,14 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  render() {
+
+    let name = 'Isaac'
+
+    return (
+      <View style={styles.container}>
+        <Text>Hello World!</Text>
+        <Image 
+          source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_font_awesome.svg/2048px-User_font_awesome.svg.png'}}
+          style={{ width: 40, height: 40, marginTop: 10 }}
+        />
+        <Text style={{ color: '#33FF', fontSize: 20 }}> {/* ou '20px' */}
+          <b>{name}</b> is a programmer?
+        </Text>
+      </View>
+    );
+  }
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
