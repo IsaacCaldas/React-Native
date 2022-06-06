8f8860ec5f045a6e9b4e703802ab699f6d39ea89
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-06-06 10:56:30 -0300

    Finished Navigations

--- section-7/RoutersNative/src/components/CustomDrawer.js
+++ section-7/RoutersNative/src/components/CustomDrawer.js
@@ -1,9 +1,29 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
-export default function CustomDrawer() {
+import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer'
+
+export default function CustomDrawer(props) {
   return (
-    <View>
-      <Text>CustomDrawer</Text>
-    </View>
+    <DrawerContentScrollView {...props}>
+      <View style={{
+          width: '100%',
+          height: 85,
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginTop: 30,
+      }}>
+        <Image
+          source={require('../assets/images/user.png')}
+          style={{
+            width: 50,
+            height: 50
+          }}
+        />
+        <Text style={{color: '111', fontSize: 17, marginTop: 10}}>
+          Welcome!
+        </Text>
+      </View>
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
   );
 }
