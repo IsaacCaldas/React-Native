1104b11f6ff243570a31c9adc7228bb8196e7b8a
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-06-06 10:35:44 -0300

    Drawer Nav

--- section-7/RoutersNative/src/routes/index.js
+++ section-7/RoutersNative/src/routes/index.js
@@ -1,6 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 
-import FontAwesome from 'react-native-vector-icons/FontAwesome'
 import Feather from 'react-native-vector-icons/Feather'
 import AntDesign from 'react-native-vector-icons/AntDesign'
 
@@ -12,48 +12,64 @@ const Tab = createBottomTabNavigator()
 
 export default function Routes() {
 
+  const Drawer = createDrawerNavigator()
+
   return (
-    <Tab.Navigator
-      screenOptions={{
-        headerShown: false,
-        tabBarHideOnKeyboard: true,
-        tabBarShowLabel: false,
-        tabBarActiveTintColor: '#bf33a3',
-        tabBarInactiveTintColor: '#ccc',
-        tabBarStyle: {
-          backgroundColor: '#333',
-          shadowOffset: { width: 0, height: 0 },
-          borderTopWidth: 0,
-          shadowColor: '#000',
-          shadowOpacity: 0.2,
-          elevation: 5,
-        }
-      }}
-    >
-      <Tab.Screen name="HomeStack" component={stackRoutes}
-        options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({ color, size }) => (
-            <AntDesign name="home" color={color} size={size} />
-          )
-        }}
+    <Drawer.Navigator>
+      <Drawer.Screen 
+        name="HomeStack"
+        component={stackRoutes}
       />
-      <Tab.Screen name="About" component={About}
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => (
-            <Feather name="info" color={color} size={size} />
-          )
-        }} 
+      <Drawer.Screen
+        name="About"
+        component={About}
+      /> 
+      <Drawer.Screen
+        name="Contacts" 
+        component={Contacts}
       />
-      <Tab.Screen name="Contacts" component={Contacts}
-        options={{
-          tabBarLabel: 'Contatos',
-          tabBarIcon: ({ color, size }) => (
-            <AntDesign name="contacts" color={"red"} size={size} />
-          )
-        }}
-      />
-    </Tab.Navigator>
+    </Drawer.Navigator>
+    // <Tab.Navigator
+    //   screenOptions={{
+    //     headerShown: false,
+    //     tabBarHideOnKeyboard: true,
+    //     tabBarShowLabel: false,
+    //     tabBarActiveTintColor: '#bf33a3',
+    //     tabBarInactiveTintColor: '#ccc',
+    //     tabBarStyle: {
+    //       backgroundColor: '#333',
+    //       shadowOffset: { width: 0, height: 0 },
+    //       borderTopWidth: 0,
+    //       shadowColor: '#000',
+    //       shadowOpacity: 0.2,
+    //       elevation: 5,
+    //     }
+    //   }}
+    // >
+    //   <Tab.Screen name="HomeStack" component={stackRoutes}
+    //     options={{
+    //       tabBarLabel: 'Home',
+    //       tabBarIcon: ({ color, size }) => (
+    //         <AntDesign name="home" color={color} size={size} />
+    //       )
+    //     }}
+    //   />
+    //   <Tab.Screen name="About" component={About}
+    //     options={{
+    //       headerShown: false,
+    //       tabBarIcon: ({ color, size }) => (
+    //         <Feather name="info" color={color} size={size} />
+    //       )
+    //     }} 
+    //   />
+    //   <Tab.Screen name="Contacts" component={Contacts}
+    //     options={{
+    //       tabBarLabel: 'Contatos',
+    //       tabBarIcon: ({ color, size }) => (
+    //         <AntDesign name="contacts" color={"red"} size={size} />
+    //       )
+    //     }}
+    //   />
+    // </Tab.Navigator>
   )
 }
