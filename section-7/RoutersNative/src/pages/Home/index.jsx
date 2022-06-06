8fbc5db18f43a92474016fb5aefcb26bed8d20e1
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-06-06 10:08:08 -0300

    Tab + Stacks

--- section-7/RoutersNative/src/pages/Home/index.jsx
+++ section-7/RoutersNative/src/pages/Home/index.jsx
@@ -1,10 +1,18 @@
 import { View, Text, StyleSheet, Button } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 
 export default function Home() {
+
+  const navigation = useNavigation()
+
+  function goToDetails() {
+    navigation.navigate('Details')
+  }
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home Page</Text>
+      <Button title="Go to Details" onPress={goToDetails} />
     </View>
   )
 }
---
+++ section-7/RoutersNative/src/routes/stackRoutes.js
@@ -0,0 +1,18 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "../pages/Home";
+import Details from "../pages/Details";
+
+const Stack = createNativeStackNavigator();
+
+export default function StackRoutes() {
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} 
+        options={{
+          headerShown: false
+        }}
+      />
+      <Stack.Screen name="Details" component={Details} />
+    </Stack.Navigator>
+  )
+}
